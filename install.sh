10ec84ae9778227cc1bc4606528e85164f6194eb
Shaheen Ghiassy <shaheen@groupon.com>
2018-06-03 12:27:43 -0700

    Adding general install script

--- install.sh
+++ install.sh
@@ -1,22 +1,37 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
-# Set source and target directories
-powerline_fonts_dir="$(cd "$(dirname "$0")" && pwd)"
+# install homebrew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-# if an argument is given it is used to select which fonts to install
-prefix="$1"
+# Install base packages
+brew install git wget
 
-# MacOS
-font_dir="$HOME/Library/Fonts"
+# Install FZF, Z & FZ
+brew install fzf
+mkdir ~/.bash_completion.d
+curl "https://raw.githubusercontent.com/rupa/z/master/{z.sh}" -o ~/.bash_completion.d/"#1"
+curl "https://raw.githubusercontent.com/changyuheng/fz/master/{fz.sh}" -o ~/.bash_completion.d/z"#1"
+reload
 
-# Copy all fonts to user fonts directory
-echo "Copying fonts..."
-find "$powerline_fonts_dir" \( -name "$prefix*.[ot]tf" -or -name "$prefix*.pcf.gz" \) -type f -print0 | xargs -0 -n1 -I % cp "%" "$font_dir/"
+# Install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+reload
+nvm install 9
 
-# Reset font cache on Linux
-if which fc-cache >/dev/null 2>&1; then
-	echo "Resetting font cache, this may take a moment..."
-	fc-cache -f "$font_dir"
-fi
+# Install SDKMan
+curl -s "https://get.sdkman.io" | bash
+reload
+sdkman install java
 
-echo "Powerline fonts installed to $font_dir"
+brew install goenv
+brew install rbenv
+brew install emacs --with-cocoa --with-gnutls --with-rsvg --with-imagemagick
+
+brew install kylef/formulae/podenv
+podenv install 1.5.2
+
+# Install Emacs
+brew install emacs --with-cocoa --with-gnutls --with-rsvg --with-imagemagick
+
+# Install fonts
+sh ./fonts/install.sh
